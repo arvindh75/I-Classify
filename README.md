0ae462d6f378d0dfcd48c6e05b6cf77f2e0af7d4
arvindh75 <iiith@arvindharun.com>
2020-12-04 12:22:30 +0530

    More options in script

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-##Instructions to run
-- `mkdir data`
-- `cd data && git clone https://github.com/pjreddie/darknet.git`
-- ``
-- ``
-- ``
+## Instructions to run
+- `bash script.sh <tiny/large/both>`
+> Tiny uses yolov3-tiny weights (~30mb) while large uses yolov3 weights (~300mb).
+Ex : `bash script.sh tiny` 
+Default option is tiny.
+- ```python
+i = "python main.py"
+print i
+ ```
--- script.sh
+++ script.sh
@@ -1,8 +1,38 @@
+weight=0
+var1="tiny"
+var2="large"
+var3="both"
+
+if [ $# -gt 0 ]
+then
+    if [ "$1" == "$var1" ]
+    then
+        weight=0
+    elif [ "$1" == "$var2" ]
+    then
+        weight=1
+    elif [ "$1" == "$var3" ]
+    then
+        weight=2
+    else
+        weight=0
+    fi
+fi
+
 repo="https://github.com/pjreddie/darknet.git"
 mkdir "data"
 cd "data"
 git clone "$repo"
 cd "darknet"
+if [ $weight -lt 1 ]
+then
+    wget "https://pjreddie.com/media/files/yolov3-tiny.weights"
+elif [$weight -gt 1]
+then
+    wget "https://pjreddie.com/media/files/yolov3.weights"
+    wget "https://pjreddie.com/media/files/yolov3-tiny.weights"
+else
+    wget "https://pjreddie.com/media/files/yolov3.weights"
+fi
 wget "https://pjreddie.com/media/files/darknet19.weights"
-wget "https://pjreddie.com/media/files/yolov3-tiny.weights"
 cd ../../
